111d3f767fd54bad63e4af53acc479b8097e07a5
unknown <mattcmej@gmail.com>
2018-07-24 02:31:25 -0700

    Begin making file structure and 'MainRouter' and 'AppNavbar' files.

---
+++ client/src/MainRouter.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container, Jumbotron} from "reactstrap";
+
+
+const App = () => {
+    <div>
+        <Router>
+            <div>
+                <AppNavbar/>
+            </div>
+        </Router>
+        
+    </div>
+};
