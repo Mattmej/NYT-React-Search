d497c3b27471406835a8d7ea043e4a8cff70b6b0
unknown <mattcmej@gmail.com>
2018-07-25 20:36:14 -0700

    Add 'save article' button to each article card.

--- client/src/components/ArticleDisplay/Article.js
+++ client/src/components/ArticleDisplay/Article.js
@@ -1,19 +1,22 @@
 import React from "react";
+import {Button} from "reactstrap";
 
 // class Article extends React.Component {
 const Article = (props) => (
+   <div className="card mb-4">
+        <div className = "card-header bg-info">
+            {/* <div> */}
+            <h3>
+                {props.title}
+                <Button color="success" className="float-right">Save Article</Button>
+            </h3>
+            {/* </div> */}
+        </div>
 
-            <div className="card mb-4">
-                <div className = "card-header bg-info">
-                    <h3>{props.title}</h3>
-                </div>
-
-                <div className = "card-body">
-                    <p>{props.summary}</p>
-                </div>
-            </div>
-
-
+        <div className = "card-body">
+            <p>{props.summary}</p>
+        </div>
+    </div>
 );
 // };
 
